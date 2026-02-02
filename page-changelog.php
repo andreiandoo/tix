ee83cc08a8c2ff3686d8dfc95e0fc5af15a1875a
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2026-02-02 15:22:12 +0200

    Changes

--- page-changelog.php
+++ page-changelog.php
@@ -7,9 +7,9 @@ get_header();
 ?>
 
 <div class="container mx-auto px-4 py-8 mt-16">
-    <?php echo do_shortcode('[epas_changelog show_summary="true" group_by="module" type="feat"]'); ?>
+    <?php echo do_shortcode('[epas_changelog show_summary="true" group_by="module"]'); ?>
 
-    <?php //echo do_shortcode('[epas_changelog show_summary="true" group_by="module"]'); ?>
+    <?php //echo do_shortcode('[epas_changelog show_summary="true" group_by="module" type="feat"]'); ?>
 </div>
 
 
